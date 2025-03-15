92a2839980d4f3b4dc81f80251cf7b6b3c506928
Yichen Shuai <2992894S@student.gla.ac.uk>
2025-03-15 23:04:13 +0000

    combine with shuju brench (new db.sqlite3 file), and fix the cart urls

--- shopping_platform/cart/urls.py
+++ shopping_platform/cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('view_cart/', views.view_cart, name='view_cart'),
+    path('remove_from_cart/<int:cart_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update_cart/<int:cart_id>/', views.update_cart, name='update_cart'),
 ]
